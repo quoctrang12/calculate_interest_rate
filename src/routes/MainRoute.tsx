52433662233721b5459e05b8f654ae70cfb05a2c
quoctrang12 <waipayttt@gmail.com>
2025-02-20 23:00:19 +0700

    update wedding page

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import App from "../App";
 import { HomePage } from "../pages";
+import WeddingPage from "../pages/wedding";
 
 export default function MainRoute(){
 
@@ -9,6 +10,7 @@ export default function MainRoute(){
             <Routes >
                 <Route path="/" element={<App/>}>
                     <Route path="/" element={<HomePage/>} />
+                    <Route path="wedding" element={<WeddingPage/>} />
                     <Route path="/*" element={<Navigate to={"/"}/>} />
                 </Route>
             </Routes>
